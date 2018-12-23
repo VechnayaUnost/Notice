91dc4c4a8672300dfa874b0dbcc173681bade188
VechnayaUnost <zditovetskaya.d@yandex.ru>
2018-12-24 00:00:18 +0300

    add BottomAppBar

--- app/src/main/java/by/darya/zdzitavetskaya/notice/MainActivity.java
+++ app/src/main/java/by/darya/zdzitavetskaya/notice/MainActivity.java
@@ -1,14 +1,28 @@
 package by.darya.zdzitavetskaya.notice;
 
+import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.DecelerateInterpolator;
 
 import com.crashlytics.android.Crashlytics;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import by.darya.zdzitavetskaya.notice.ui.fragment.NoticeFragment;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_app_bar)
+    BottomAppBar bottomAppBar;
+
+    @OnClick(R.id.fab)
+    public void fabClick() {
+        setFabAlignmentMode();
+        bottomAppBar.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2));
+    }
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -16,9 +30,21 @@ public class MainActivity extends AppCompatActivity {
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
+        setSupportActionBar(bottomAppBar);
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.main_container_frame_layout, new NoticeFragment())
+                .add(R.id.frame_container, new NoticeFragment())
                 .commit();
+    }
+
+    public void setFabAlignmentMode() {
+        if (bottomAppBar.getFabAlignmentMode() == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER) {
+            bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_END);
+        } else {
+            bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
+        }
     }
 }
